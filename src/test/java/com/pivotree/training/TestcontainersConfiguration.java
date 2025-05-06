e765dd5e97878a110a5fa76d38157ca459890c1d
sauravsuman378 <saurav.suman@pivotree.com>
2025-05-06 14:15:37 +0530

    Hello world file

--- src/test/java/com/pivotree/training/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.pivotree.training;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
